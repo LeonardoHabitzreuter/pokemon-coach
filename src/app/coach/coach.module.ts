64092fb5b203631c750498ddaa1bf58ac849d955
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-11-10 12:40:13 -0200

    configured lazy loading for the coach module

--- src/app/coach/coach.module.ts
+++ src/app/coach/coach.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from 'shared/module'
+import { CoachComponent } from './coach.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild([{path: 'coach', component: CoachComponent}])
   ],
-  declarations: []
+  declarations: [CoachComponent]
 })
 export class CoachModule { }
