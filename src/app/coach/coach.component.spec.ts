a3ac9b08c5e81d142e884e7b28d7a782a7652a7e
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-11-12 19:41:29 -0200

    fixed coach component tests

--- src/app/coach/coach.component.spec.ts
+++ src/app/coach/coach.component.spec.ts
@@ -1,3 +1,7 @@
+import { Component, Input } from '@angular/core'
+import { PokemonsService } from 'app/pokemons/pokemons.service'
+import { MessageService } from 'primeng/api'
+import { ConfirmationService } from 'primeng/api'
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoachComponent } from './coach.component';
@@ -5,10 +9,24 @@ import { CoachComponent } from './coach.component';
 describe('CoachComponent', () => {
   let component: CoachComponent;
   let fixture: ComponentFixture<CoachComponent>;
+  const confirmationServiceStub: Partial<ConfirmationService> = {
+
+  }
+  const pokemonsServiceStub: Partial<PokemonsService> = {
+    getFavoritePokemons: jest.fn().mockResolvedValue([])
+  }
+  const messageServiceStub: Partial<MessageService> = {
+
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoachComponent ]
+      declarations: [ CoachComponent, AppContentHeaderComponent, TableComponent ],
+      providers: [
+        {provide: ConfirmationService, useValue: confirmationServiceStub },
+        {provide: PokemonsService, useValue: pokemonsServiceStub },
+        {provide: MessageService, useValue: messageServiceStub }
+      ]
     })
     .compileComponents();
   }));
@@ -23,3 +41,19 @@ describe('CoachComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'app-content-header',
+  template: ''
+})
+class AppContentHeaderComponent {
+  @Input() title: string
+}
+
+@Component({
+  selector: 'p-table',
+  template: ''
+})
+class TableComponent {
+  @Input() value: any[]
+}
